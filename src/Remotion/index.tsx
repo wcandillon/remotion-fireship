6b6b04f0d1f1dfcaaf1b140f6c2597e84a20117c
William Candillon <wcandillon@gmail.com>
2022-02-18 16:07:27 +0100

    :wrench:

--- src/Remotion/index.tsx
+++ src/Remotion/index.tsx
@@ -1,15 +1,19 @@
 import { Audio, Sequence, Series, staticFile, Video } from "remotion";
+import { View } from "react-native";
 
 import { WeatherMap } from "./components/WeatherMap/WeatherMap";
 import { Flipbook } from "./Flipbook";
 import { HdTo4k } from "./helpers/hd-to-4k";
 import { UsingJavaScript } from "./UsingJavaScript";
 import { Reactive } from "./Reactive";
+import { CANVAS } from "./components/Canvas";
 
 const audio = staticFile("/audio.wav");
 const video = staticFile("/videos/video.mov");
 const usingJS = staticFile("/videos/using-javascript.mov");
 const flipbook = staticFile("/videos/flipbook.mov");
+const videoInReact = staticFile("/videos/video-in-react.mov");
+const introducingRemotion = staticFile("/videos/introducing-remotion.mov");
 
 export const Remotion = () => {
   return (
@@ -32,6 +36,21 @@ export const Remotion = () => {
         </Series.Sequence>
         <Series.Sequence durationInFrames={90}>
           <Reactive />
+        </Series.Sequence>
+        <Series.Sequence durationInFrames={90}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "#282C34",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Video src={videoInReact} height={CANVAS.height} />
+          </View>
+        </Series.Sequence>
+        <Series.Sequence durationInFrames={42}>
+          <Video src={introducingRemotion} height={CANVAS.height} />
         </Series.Sequence>
       </Series>
       <Sequence from={1250} durationInFrames={200}>
