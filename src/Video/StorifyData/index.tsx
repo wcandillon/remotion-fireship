565c68f8cf65a7c3e5dfc54c2430ca831c5d5a83
Jonny Burger <jonathanburger11@gmail.com>
2022-02-25 10:43:38 +0100

    Update index.tsx

--- src/Video/StorifyData/index.tsx
+++ src/Video/StorifyData/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { AbsoluteFill, Sequence } from "remotion";
 
+import { BG_COLOR2 } from "../helpers/colors";
+
 import { JsonData } from "./JsonData";
 import { Player } from "./Player";
 
 export const StorifyData: React.FC = () => {
   return (
     <AbsoluteFill
-      style={{ backgroundColor: "#221d28", width: 1920, height: 1080 }}
+      style={{ backgroundColor: BG_COLOR2, width: 1920, height: 1080 }}
     >
       <JsonData />
       <AbsoluteFill
