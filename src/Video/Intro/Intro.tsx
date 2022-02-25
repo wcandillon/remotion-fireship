169fa53a012401dc2264f6c38ada88894f6885ce
Jonny Burger <jonathanburger11@gmail.com>
2022-02-25 14:44:55 +0100

    Update Intro.tsx

--- src/Video/Intro/Intro.tsx
+++ src/Video/Intro/Intro.tsx
@@ -1,7 +1,7 @@
 import { first } from "lodash";
 import type { FC } from "react";
 import { useState } from "react";
-import { interpolate, spring, useVideoConfig } from "remotion";
+import { AbsoluteFill, interpolate, spring, useVideoConfig } from "remotion";
 import styled from "styled-components";
 
 import { FakeDevTools } from "../FakeDevTools";
@@ -66,6 +66,14 @@ export const Intro: FC<{ frame: number }> = ({ frame }) => {
       damping: 200,
     },
   });
+  const sourceCode = spring({
+    fps,
+    frame: frame - 60,
+    config: {
+      stiffness: 100,
+      damping: 200,
+    },
+  });
   const offset1 = interpolate(spring1, [0, 1], [1080, 0]);
   const offset2 = interpolate(spring2, [0, 1], [1080, 0]);
 
@@ -94,57 +102,82 @@ export const Intro: FC<{ frame: number }> = ({ frame }) => {
   return (
     <Container
       style={{
-        transform: `scale(${scale})`,
         opacity,
         background: BG_COLOR2,
         color: "white",
       }}
     >
-      <svg
+      <AbsoluteFill
         style={{
-          width,
-          height,
-          position: "absolute",
+          transform: `scale(${scale})`,
         }}
       >
-        <defs>
-          <linearGradient id="lg">
-            <stop stopColor="#326cb8" offset="0" />
-            <stop stopColor="#2fa8b1" offset="1" />
-          </linearGradient>
-          <mask id="mask">{arcs}</mask>
-        </defs>
-        {arcs}
-      </svg>
-      <ZIndex1 style={{ color: "white", fontFamily: "Cubano" }}>
-        <div style={{ transform: `translateY(${offset1}px)` }}>
-          <Text>This</Text>
-          <Text> </Text>
-          <Text>video </Text>
-          <Text>is</Text>
+        <svg
+          style={{
+            width,
+            height,
+            position: "absolute",
+          }}
+        >
+          <defs>
+            <linearGradient id="lg">
+              <stop stopColor="#326cb8" offset="0" />
+              <stop stopColor="#2fa8b1" offset="1" />
+            </linearGradient>
+            <mask id="mask">{arcs}</mask>
+          </defs>
+          {arcs}
+        </svg>
+        <ZIndex1 style={{ color: "white", fontFamily: "Cubano" }}>
+          <div style={{ transform: `translateY(${offset1}px)` }}>
+            <Text>This</Text>
+            <Text> </Text>
+            <Text>video </Text>
+            <Text>is</Text>
+          </div>
+          <div style={{ transform: `translateY(${offset2}px)` }}>
+            <Text>made </Text>
+            <FakeDevTools
+              width={100.25}
+              height={35.1}
+              clName="sc-dJjYzT"
+              enabled={firstEnabled}
+            >
+              <Text>with</Text>
+            </FakeDevTools>
+            <Text> </Text>
+            <FakeDevTools
+              width={125.44}
+              height={35.1}
+              clName="sc-fotOHu"
+              enabled={secondEnabled}
+            >
+              <Text>React</Text>
+            </FakeDevTools>
+          </div>
+        </ZIndex1>
+        <MouseCursor />
+      </AbsoluteFill>
+      <AbsoluteFill
+        style={{
+          position: "absolute",
+          fontFamily: "Cubano",
+          color: "white",
+          fontSize: 100,
+        }}
+      >
+        <div
+          style={{
+            bottom: 200,
+            position: "absolute",
+            textAlign: "center",
+            width: "100%",
+            opacity: sourceCode,
+          }}
+        >
+          Source code in the description
         </div>
-        <div style={{ transform: `translateY(${offset2}px)` }}>
-          <Text>made </Text>
-          <FakeDevTools
-            width={100.25}
-            height={35.1}
-            clName="sc-dJjYzT"
-            enabled={firstEnabled}
-          >
-            <Text>with</Text>
-          </FakeDevTools>
-          <Text> </Text>
-          <FakeDevTools
-            width={125.44}
-            height={35.1}
-            clName="sc-fotOHu"
-            enabled={secondEnabled}
-          >
-            <Text>React</Text>
-          </FakeDevTools>
-        </div>
-      </ZIndex1>
-      <MouseCursor />
+      </AbsoluteFill>
     </Container>
   );
 };
